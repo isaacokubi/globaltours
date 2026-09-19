199a869d09379bc5e4091f18d90ba6c5fb16aad1
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-20 01:03:39 +0300

    Add Flutter customer screens lib/ui/screens/bookings/my_bookings_screen.dart

---
+++ lib/ui/screens/bookings/my_bookings_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../providers/booking_provider.dart';
+import '../../widgets/app_shell.dart';
+class MyBookingsScreen extends StatefulWidget { const MyBookingsScreen({super.key}); @override State<MyBookingsScreen> createState()=>_MyBookingsScreenState(); }
+class _MyBookingsScreenState extends State<MyBookingsScreen>{@override void initState(){super.initState();WidgetsBinding.instance.addPostFrameCallback((_)=>context.read<BookingProvider>().load());}
+ @override Widget build(BuildContext context){final p=context.watch<BookingProvider>();return AppShell(child:p.loading?const Center(child:CircularProgressIndicator()):RefreshIndicator(onRefresh:p.load,child:ListView.builder(padding:const EdgeInsets.all(16),itemCount:p.bookings.length,itemBuilder:(_,i){final b=p.bookings[i];return Card(child:ListTile(title:Text('Booking '+(b['bookingNumber']??b['_id']??'').toString()),subtitle:Text('Status: '+(b['status']??'pending').toString())));})));}}
