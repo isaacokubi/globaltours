57987fdb5f91624ee1cdeff2819d072bddf5dcb5
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-20 01:01:57 +0300

    Initialize Global Tours Flutter: lib/core/routing/app_router.dart

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../../ui/screens/home/home_screen.dart';
+import '../../ui/screens/auth/login_screen.dart';
+import '../../ui/screens/tours/tours_screen.dart';
+import '../../ui/screens/tours/tour_detail_screen.dart';
+import '../../ui/screens/bookings/my_bookings_screen.dart';
+import '../../ui/screens/admin/admin_dashboard_screen.dart';
+import '../../ui/screens/admin/finance_screen.dart';
+import '../../ui/screens/profile/profile_screen.dart';
+
+class AppRouter {
+  static const home = '/';
+  static const login = '/login';
+  static const tours = '/tours';
+  static const tour = '/tour';
+  static const bookings = '/bookings';
+  static const admin = '/admin';
+  static const finance = '/admin/finance';
+  static const profile = '/profile';
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case home: return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case login: return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case tours: return MaterialPageRoute(builder: (_) => const ToursScreen());
+      case tour: return MaterialPageRoute(builder: (_) => TourDetailScreen(tour: settings.arguments as Map<String, dynamic>));
+      case bookings: return MaterialPageRoute(builder: (_) => const MyBookingsScreen());
+      case admin: return MaterialPageRoute(builder: (_) => const AdminDashboardScreen());
+      case finance: return MaterialPageRoute(builder: (_) => const FinanceScreen());
+      case profile: return MaterialPageRoute(builder: (_) => const ProfileScreen());
+      default: return MaterialPageRoute(builder: (_) => const HomeScreen());
+    }
+  }
+}
